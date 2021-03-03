80708ef4a330857711eb2fc9916681dafb608a47
Will Allan <will.allan@disneystreaming.com>
2021-03-03 09:40:52 -0800

    Update comment

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ export default async function startServer(dev) {
   if (dev) {
     const config = snowpack.createConfiguration(snowpackUserConfig);
     const devServer = await snowpack.startServer({ config });
-    const socketServer = createSocketServer(devServer.server); // See commit in forked repo required to make this work: https://github.com/billyzkid/snowpack/commit/47a45a0aa3adb44180ec832c6f6f934b03cf8ac2
+    const socketServer = createSocketServer(devServer.server); // See PR required to make this work: https://github.com/snowpackjs/snowpack/pull/2778
   } else {
     const app = express();
     const httpServer = http.createServer(app);
